5a25dc218e93e632baac3d52974d2b2b08152d80
Manuel <Mvelazquezhu2024@fau.edu>
2026-01-20 22:58:10 -0500

    commit

--- games.js
+++ games.js
@@ -1,104 +1,90 @@
-// Image credits: Kickstarter
-const games = ` [
-  {
-    "name": "Heroes Of Mythic Americas",
-    "description": "An exciting 5e RPG supplement that heroically represents pre-Columbian American cultures and mythologies",
-    "pledged": 1572,
-    "goal": 10000,
-    "backers": 9,
-    "img": "./assets/heroes_of_mythic_americas.png"
-  },
-  {
-    "name": "Cube Monster",
-    "description": "Be the first champion to reach the top of Mount Kubia in a solo or competitive engine building strategy game",
-    "pledged": 29446,
-    "goal": 20000,
-    "backers": 321,
-    "img": "./assets/cube_monster.png"
-  },
-  {
-    "name": "Zoo Tycoon: The Board Game",
-    "description": "Your zoo in a box with more than 230 Animal Meeples. A highly thematic board game experience for 1-4 players.",
-    "pledged": 442602,
-    "goal": 78480,
-    "backers": 3869,
-    "img": "./assets/zoo_tycoon.png"
-  },
-  {
-    "name": "Deity Tarot",
-    "description": "A fully illustrated 78-card tarot deck with a divinely exalted take on the famous Smith-Waite tarot deck.",
-    "pledged": 109,
-    "goal": 8000,
-    "backers": 3,
-    "img": "./assets/deity_tarot.png"
-  },
-  {
-    "name": "Camouflage | A hand-painting puzzle/adventure game",
-    "description": "Paint your own camouflage to retrieve information, access secret locations and remove the threat to your fellow flying squirrels.",
-    "pledged": 698,
-    "goal": 5140,
-    "backers": 9,
-    "img": "./assets/camouflage.png"
-  },
-  {
-    "name": "Beep Bapp Boom",
-    "description": "The time to explode is getting shorter and shorter, please be alert!",
-    "pledged": 44,
-    "goal": 18133,
-    "backers": 2,
-    "img": "./assets/beep_bapp_boom.png"
-  },
-  {
-    "name": "Frosthaven",
-    "description": "Euro-inspired dungeon crawling sequel to the 2017 smash hit Gloomhaven",
-    "pledged": 69608,
-    "goal": 500000,
-    "backers": 3193,
-    "img": "./assets/frosthaven.png"
-  },
-  {
-    "name": "Mislight - An Adventure Game With A Small Touch Of Thriller",
-    "description": "Be the Light in a realm of Darkness",
-    "pledged": 1036,
-    "goal": 3099,
-    "backers": 32,
-    "img": "./assets/mislight.png"
-  },
-  {
-    "name": "How to Read Minds 2 Kit: Ellusionist x Peter Turner",
-    "description": "Command Attention With These REAL Mind Reading Techniques That Anyone Can Do... The Contents Inside This Kit Will Silence Skeptics.",
-    "pledged": 147975,
-    "goal": 10000,
-    "backers": 1039,
-    "img": "./assets/how_to_read_minds_2.png"
-  },
-  {
-    "name": "A Wayfarer's Tale",
-    "description": "A Wayfarer's Tale, is a Solo to 4 player game where you explore uncharted islands, collecting Treasure while avoiding Monsters.",
-    "pledged": 13039,
-    "goal": 1183,
-    "backers": 1446,
-    "img": "./assets/wayfarers_tale.png"
-  },
-  {
-    "name": "Kingdom Death: Monster 1.5",
-    "description": "A cooperative nightmare horror game experience",
-    "pledged": 94139,
-    "goal": 100000,
-    "backers": 9264,
-    "img": "./assets/kingdom_death.png"
-  }
-]
-`
-const template = `
-{
-"name": "",
-"description": "",
-"pledged": 0,
-"goal": 0,
-"backers": 0,
-"img": ""
+const games = `[{
+  "name":"Sea Monster",
+  "description":"Dive into the depths and hunt down the Sea Monster in this thrilling adventure.",
+  "pledged": 21423,
+  "goal": 50000,
+  "backers": 1924,
+  "img":"./assets/tentacles.png"
 },
-`
+{
+  "name":"Zoo Tycoon: The Board Game",
+  "description":"A board game adaptation of the classic Zoo Tycoon franchise.",
+  "pledged": 152540,
+  "goal": 50000,
+  "backers": 4621,
+  "img":"./assets/zoo_tycoon.png"
+},
+{
+  "name":"How to Read Minds 2 Kit: Ellusionist x Peter Turner",
+  "description":"A premium mentalism kit and learning experience.",
+  "pledged": 113900,
+  "goal": 30000,
+  "backers": 3180,
+  "img":"./assets/how_to_read_minds_2.png"
+},
+{
+  "name":"The Fabled Lands",
+  "description":"An epic fantasy campaign game with rich storytelling.",
+  "pledged": 89400,
+  "goal": 100000,
+  "backers": 2021,
+  "img":"./assets/camouflage.png"
+},
+{
+  "name":"Dungeon Draw",
+  "description":"A fast-paced dungeon crawler card game.",
+  "pledged": 44200,
+  "goal": 50000,
+  "backers": 1310,
+  "img":"./assets/cube_monster.png"
+},
+{
+  "name":"Skyward Isles",
+  "description":"Explore floating islands and build your airship crew.",
+  "pledged": 32100,
+  "goal": 60000,
+  "backers": 948,
+  "img":"./assets/beep_bapp_boom.png"
+},
+{
+  "name":"Pixel Pirates",
+  "description":"Retro pirate strategy game with pixel art style.",
+  "pledged": 15800,
+  "goal": 25000,
+  "backers": 604,
+  "img":"./assets/deity_tarot.png"
+},
+{
+  "name":"Neon Ninjas",
+  "description":"A cyberpunk stealth game featuring neon-lit cityscapes.",
+  "pledged": 21900,
+  "goal": 45000,
+  "backers": 798,
+  "img":"./assets/mislight.png"
+},
+{
+  "name":"Castle Siege",
+  "description":"A tactical siege simulator with co-op gameplay.",
+  "pledged": 38800,
+  "goal": 75000,
+  "backers": 1227,
+  "img":"./assets/heroes_of_mythic_americas.png"
+},
+{
+  "name":"Starlight Speedway",
+  "description":"A space racing game featuring drifting and boosters.",
+  "pledged": 17300,
+  "goal": 40000,
+  "backers": 507,
+  "img":"./assets/wayfarers_tale.png"
+},
+{
+  "name":"Wizards of the Wastes",
+  "description":"Battle for magical dominance in a harsh wasteland.",
+  "pledged": 73800,
+  "goal": 90000,
+  "backers": 1947,
+  "img":"./assets/kingdom_death.png"
+}]`;
 
 export default games;
